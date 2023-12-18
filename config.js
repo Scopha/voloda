be34b1a3a5c14f73a7567552d3402f2fee1de624
Scopha <scopha@scopha.com>
2023-12-18 23:36:50 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  //server database
+  MONGODB_USERNAME: "voloda",
+  MONGODB_PASSWORD: "x5IqUhUw0HWLCXrO",
+  MONGODB_DB_NAME: "test",
+
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "XHqPKJs89B",
+
+  //gmail credentials for send email
+  EMAIL: "sckodnet@gmail.com",
+  PASSWORD: "Educdz2008",
+
+  //secret key for jwt
+  JWT_SECRET: "utbi9qvix0",
+
+  //baseURL
+  baseURL: "http://voloda.net/",
+
+  //firebase server key for notification
+  SERVER_KEY:
+    "AAAAH4GSwYY:APA91bFk3_N5ezm2RCdPbNRfdQtJ5S8eHI27t5fA1zrEoi9GkQf_9H9-b6swE0ElrZl4XcsH_8axdJrWtLSYAsPmG1qn3gbyc9OUaujHlbekNXGfVKS7kw3LxqShE3_TmHGNOIzISGgN",
+};
